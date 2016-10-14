0e1afca66d2aa0fb2c210d049d71bdb6b6bce78f
Tom Brown <40210205@live.napier.ac.uk>
2016-10-14 12:44:39 +0000

    minor debug issue fixed

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 
-flask import Flask
-app = Flask ( __name__ )
+from flask import Flask
+app = Flask(__name__)
 
-@app.route ("/")
+@app.route("/")
 def home():
-  return " Hello World !"
+  return "Hello World!"
 
-if __name__ == " __main__ ":
-  app.run( host ='0.0.0.0 ')
+if __name__ == "__main__":
+  app.run(host ='0.0.0.0')
