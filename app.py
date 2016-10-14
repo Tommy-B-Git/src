b395dd942d849e5c5d6ead9f2a869a055d8da26b
Tom Brown <40210205@live.napier.ac.uk>
2016-10-14 14:39:30 +0000

    Added dynamic route for genres

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 
-from flask import Flask
+from flask import Flask, redirect, url_for, abort, request
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def home():
-  return "Hello World!"
+  return 'Home page'
+
+@app.route('/genre/<query>')
+def genre(query):
+  return 'This is the %s page' % query
+
+
 
 if __name__ == "__main__":
-  app.run(host ='0.0.0.0')
+  app.run(host='0.0.0.0', debug=True)
+
