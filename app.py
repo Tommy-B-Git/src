8bc278ca706c77ae89c7ef2078270293c7434596
Tom Brown <40210205@live.napier.ac.uk>
2016-10-25 16:17:35 +0000

    Added custom 404 route

--- app.py
+++ app.py
@@ -76,7 +76,7 @@ def force404():
 
 @app.errorhandler(404)
 def page_not_found(error):
-  return "Could not find the page you requested.", 404
+  return render_template('404.html'), 404
 
 if __name__ == "__main__":
     app.run(host = "0.0.0.0", debug = True)
